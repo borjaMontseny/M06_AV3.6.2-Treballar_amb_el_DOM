89806ae5d6279ca35ab221c11a81c2df650f9c33
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2024-01-08 21:16:08 +0100

    Commit 2

--- xmas.js
+++ xmas.js
@@ -1,16 +1,27 @@
 window.onload = function () {
 
+    var correntElectrica = true;
+
     var elementsElectrics = document.querySelectorAll('.yellow, .blue');
 
-    var stars = document.querySelectorAll('.tree .star');
+    var stars = document.querySelectorAll('.tree .star .yellow');
 
     var bombetes = document.querySelectorAll(':not(.star) > .yellow, .blue');
 
+    var forks = document.querySelectorAll('.fork');
+
+    var pots = document.querySelectorAll('.pot');
+
+    var segonPot = pots[1];
+
+    var tercerPot = pots[2];
+
     // primer punt
+    crearRegals();
     hoverCanviarColorBombetes();
     hoverCanviarColorStar();
 
-    // listener per a escoltar al teclat
+    // listener per escoltar al teclat
     document.body.addEventListener("keydown", pressKey);
     // switch que administra la funció del listener ↑
     function pressKey(event) {
@@ -18,31 +29,40 @@ window.onload = function () {
 
         switch (pressedKey) {
             case "Q": // segon punt
+                console.log("Q");
                 interruptorElectricitat();
                 break;
-            case "G":
+            case "G": // tercer punt
+                console.log("G");
 
                 break;
-            case "R":
+            case "R": // quart punt
+                console.log("R");
 
                 break;
-            case "L":
-
+            case "L": // cinquè punt
+                console.log("L");
+                interruptorBombetes()
                 break;
-            case "E":
-
+            case "E": // sisè punt
+                console.log("E");
+                interruptorStar()
                 break;
-            default:
-            // fora
+            default: // fora
+                console.log("Lletra no reconeguda");
         }
     }
 
     function interruptorElectricitat() {
-        elementsElectrics.forEach(function (elemento) {
-            if (elemento.style.visibility === 'hidden' || elemento.style.visibility === '') {
-                elemento.style.visibility = 'visible';
+        elementsElectrics.forEach(function (celdaElementElectric) {
+            if (celdaElementElectric.style.visibility === 'hidden' || celdaElementElectric.style.visibility === '') {
+                celdaElementElectric.style.visibility = 'visible';
+                correntElectrica = true;
+                console.log("Ara hi ha corrent elèctrica");
             } else {
-                elemento.style.visibility = 'hidden';
+                celdaElementElectric.style.visibility = 'hidden';
+                correntElectrica = false;
+                console.log("No hi ha corrent elèctrica");
             }
         });
     }
@@ -59,13 +79,13 @@ window.onload = function () {
 
     function hoverCanviarColorBombetes() {
 
-        bombetes.forEach(function (element) {
-            element.addEventListener('mouseover', function () {
-                cambiarColor(element);
+        bombetes.forEach(function (celdaBombeta) {
+            celdaBombeta.addEventListener('mouseover', function () {
+                cambiarColor(celdaBombeta);
             });
 
-            element.addEventListener('mouseout', function () {
-                cambiarColor(element);
+            celdaBombeta.addEventListener('mouseout', function () {
+                cambiarColor(celdaBombeta);
             });
         });
     }
@@ -89,4 +109,57 @@ window.onload = function () {
         });
     }
 
+    function interruptorBombetes() {
+        if (correntElectrica) {
+            bombetes.forEach(function (bombeta) {
+                if (bombeta.style.visibility === 'hidden' || bombeta.style.visibility === '') {
+                    bombeta.style.visibility = 'visible';
+                    console.log("Has encès la bombeta");
+                } else {
+                    bombeta.style.visibility = 'hidden';
+                    console.log("Has apagat la bombeta");
+                }
+            });
+        } else if (!correntElectrica) {
+            console.log("No hi ha electricitat per encendre les bombetes");
+        }
+    }
+
+    function interruptorStar() {
+        if (correntElectrica) {
+            stars.forEach(function (celdaStar) {
+                if (celdaStar.style.visibility === 'hidden' || celdaStar.style.visibility === '') {
+                    celdaStar.style.visibility = 'visible';
+                    console.log("Has encès l'estrella");
+                } else {
+                    celdaStar.style.visibility = 'hidden';
+                    console.log("Has apagat l'estrella");
+                }
+            });
+        } else if (!correntElectrica) {
+            console.log("No hi ha electricitat per encendre la estrella");
+        }
+    }
+
+    function crearRegals() {
+
+        var divRegal = document.createElement('div');
+        divRegal.className = 'light_pixel red';
+
+        for (var i = 0; i < 3; i++) {
+            var divRegal = document.createElement('div');
+            divRegal.className = 'light_pixel red';
+
+            segonPot.appendChild(divRegal);
+        }
+
+        for (var i = 0; i < 3; i++) {
+            var divRegal = document.createElement('div');
+            divRegal.className = 'light_pixel red';
+
+            // Obtén el primer hijo del contenedor y lo inserta antes de él
+            var primerHijo = segonPot.firstChild;
+            segonPot.insertBefore(divRegal, primerHijo);
+        }
+    }
 }
